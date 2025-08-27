b2fd5ce3efac515991145b4363b389665a68ffa9
Ngọc Ánh <ADMIN@LAPTOP-F7NE9URR>
2025-08-27 22:09:46 +0700

    Add project files.

---
+++ Anh-231230714/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Anh_231230714.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+   
+            
+}
